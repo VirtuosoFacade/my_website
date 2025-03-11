a061e957b3ecea771cccb2a2cbbdbe21acc205ac
Etsu <welfareslayer@gamil.com>
2025-03-11 19:48:13 +0530

    2

--- jsFiles/jsAnimate.js
+++ jsFiles/jsAnimate.js
@@ -1,8 +1,8 @@
-const frames = ["assects/master-ball/_a_frm0,30.png", "assects/master-ball/_a_frm01,30.png", "assects/master-ball/_a_frm2,30.png",
-    "assects/master-ball/_a_frm3,30.png", "assects/master-ball/_a_frm4,30.png", "assects/master-ball/_a_frm5,30.png", "assects/master-ball/_a_frm6,30.png", "assects/master-ball/_a_frm7,30.png",
-    "assects/master-ball/_a_frm8,30.png", "assects/master-ball/_a_frm9,30.png", "assects/master-ball/_a_frm10,30.png", "assects/master-ball/_a_frm11,30.png", "assects/master-ball/_a_frm12,30.png",
-    "assects/master-ball/_a_frm13,30.png", "assects/master-ball/_a_frm14,30.png", "assects/master-ball/_a_frm15,30.png", "assects/master-ball/_a_frm16,30.png", "assects/master-ball/_a_frm17,30.png",
-    "assects/master-ball/_a_frm18,30.png", "assects/master-ball/_a_frm19,30.png", "assects/master-ball/_a_frm20,30.png"
+const frames = ["../assects/master-ball/_a_frm0,30.png", "../assects/master-ball/_a_frm01,30.png", "../assects/master-ball/_a_frm2,30.png",
+    "../assects/master-ball/_a_frm3,30.png", "../assects/master-ball/_a_frm4,30.png", "../assects/master-ball/_a_frm5,30.png", "../assects/master-ball/_a_frm6,30.png", "../assects/master-ball/_a_frm7,30.png",
+    "../assects/master-ball/_a_frm8,30.png", "../assects/master-ball/_a_frm9,30.png", "../assects/master-ball/_a_frm10,30.png", "../assects/master-ball/_a_frm11,30.png", "../assects/master-ball/_a_frm12,30.png",
+    "../assects/master-ball/_a_frm13,30.png", "../assects/master-ball/_a_frm14,30.png", "../assects/master-ball/_a_frm15,30.png", "../assects/master-ball/_a_frm16,30.png", "../assects/master-ball/_a_frm17,30.png",
+    "../assects/master-ball/_a_frm18,30.png", "../assects/master-ball/_a_frm19,30.png", "../assects/master-ball/_a_frm20,30.png"
 ]; // Array of image frames
 let currentFrame = 0;
 
